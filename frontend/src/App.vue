329a638c551b08580b6183365d7ba0b5e8b3ea19
Alp Helvaci <alphelvaci01@gmail.com>
2020-08-24 17:51:04 +0300

    Frontend bare-bones

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,16 @@
 <template>
-  <div id="app">
-    <li class="list" v-for="product in products" v-bind:key="product.id" list-style-type:none>
-      <product v-bind:caption="product.caption" v-bind:description="product.description" v-bind:url="product.url" v-bind:price="product.price"/>
-    </li>
-  </div>
+<body>
+<div id="app">
+	<li class="list" v-for="product in products" v-bind:key="product.idx" list-style-type:none>
+		<product v-bind:caption="product.name" v-bind:description="product.description" v-bind:url="'http://cdn.agbdining.com/' + product.imageUrl" v-bind:price="product.price"/>
+	</li>
+</div>
+</body>
 </template>
 
 <script>
 import Product from './components/Product.vue'
+import axios from 'axios'
 
 export default {
   name: 'App',
@@ -16,21 +19,12 @@ export default {
   },
   data() {
     return {
-      products: [
-        {
-        caption: "birinci resim",
-        description: "iwdnowidnwodinwodinwo",
-        url: "https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png",
-        price: 15
-        },
-        {
-        caption: "ikinci resim",
-        description: "iwdnowidnwodinwodinwo",
-        url:"https://images.squarespace-cdn.com/content/v1/5a5906400abd0406785519dd/1547305835578-QAFSA9Q5TND01TZBN03B/ke17ZwdGBToddI8pDm48kKAwwdAfKsTlKsCcElEApLR7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1UegTYNQkRo-Jk4EWsyBNhwKrKLo5CceA1-Tdpfgyxoog5ck0MD3_q0rY3jFJjjoLbQ/grant3.jpg?format=2500w",
-        price: 77
-        },
-      ]
+      products: null
     }
+  },
+  mounted() {
+    axios.get('http://api.agbdining.com/api/v1/products')
+      .then((response) => (this.products = response.data))
   }
 }
 </script>
@@ -44,6 +38,7 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
 .list {
   list-style-type:none
 }
--- frontend/src/components/Product.vue
+++ frontend/src/components/Product.vue
@@ -1,33 +1,76 @@
 <template>
-    <div style="width: 100%; display: table">
-        <div style="display: table-row; height: 100px;">
-            <div style="width: 33%; display: table-cell">
-                <img v-bind:src="url" height="50px" width="50px">
-            </div>
-            <div style="width: 33%; display: table-cell">
-                {{ caption }} <br>
-                {{ description }}
-            </div>
-            <div style="display: table-cell"> 
-                {{ price }}
-            </div>
-        </div>
-    </div>
+<div class="Product">
+	<div class="Product-figure">
+		<img v-bind:src="url" class="Product-image">
+	</div>
+	<div class="Product-body">
+		<h3 class="Product-title">{{ caption }}</h3>
+		<p class="Product-description">{{ description }}</p>
+		<div class="Product-Price">
+			{{ price }}
+		</div>
+	</div>
+</div>
 </template>
 
 <script>
 export default {
-    name: 'Product',
-    props:  {
-        caption: String,
-        url: String,
-        description: String,
-        price: Float32Array
-    }
-
+	name: 'Product',
+	props:  {
+		caption: String,
+		url: String,
+		description: String,
+		price: Number
+	}
 }
 </script>
 
 <style>
+
+.Product {
+	background: #fff;
+	border: 1px solid hsla(0, 0%, 0%, 0.1);
+	border-radius: 4px;
+	display: flex;
+	flex-flow: row;
+	min-width: 0;
+	overflow: hidden;
+	padding: 1em;
+	position: relative;
+	transition: flex-grow 0.5s;
+	min-width: 0;
+	flex: 5;
+	border-right: 1px solid #eee;
+	margin-bottom: 0;
+	padding-right: 1em;
+}
+
+.Product-figure {
+	width: 100%;
+	min-width: 130px;
+	border-right: 1px solid #eee;
+	flex: 2;
+	margin-right: 1em;
+	margin-bottom: 0;
+	padding-right: 1em;
+}
+
+.Product-body :last-child {
+	margin-bottom: 0;
+    min-width: 0;
+	flex: 5;
+}
+
+.Product-title {
+	font-weight: 600;
+	font-size: 1.4em;
+	margin-bottom: calc(.75em / 1.4);
+}
+
+.Product-image {
+	max-width: 120px;
+	border: 1px solid hsl(0, 0%, 90%);
+	padding: .25em;
+}
 
 </style>
